b619e63bf196b90ecafc4587d20c94c6e8473c0f
aimeemillard <53044587+aimeemillard@users.noreply.github.com>
2020-08-09 02:21:52 -0400

    update

--- Develop/routing/api-routes.js
+++ Develop/routing/api-routes.js
@@ -14,11 +14,32 @@ module.exports = function (app) {
 
   // API POST Requests
   app.post("/api/notes", function (req, res) {
+    let newNote = req.body;
+    let noteNum = data.length + 1;
+    newNote.id = noteNum;
+
+    // need to push the newNote so we can write it
+    data.push(newNote);
+
+    let parsedata = JSON.stringify(data);
+    fs.writeFile(path.join("./db/db.json"), parsedata, err => {
+      if (err) throw err;
+    });
     res.json(data);
   });
 
   // API DELETE Requests
   app.delete("/api/notes/:id", function (req, res) {
+    let noteId = re.params.id;
+    let newId = 0;
+    data = data.filter(currentNote => {
+      return currentNote.id != noteId;
+    });
+    for (currentNote of data) {
+      currentNote.id = newId.toString();
+      newId++;
+    }
+    fs.writeFileSync("./db/db.json"), parsedata;
     res.json(data);
   });
 };
